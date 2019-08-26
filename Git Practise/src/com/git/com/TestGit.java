ceef967acd25cb38ccc0950bf850bfaf817fd1c9
Palashrocker <1988.palash@gmail.com>
2019-08-26 20:03:33 +0530

    new push

---
+++ Git Practise/src/com/git/com/TestGit.java
@@ -0,0 +1,12 @@
+package com.git.com;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Another Git");
+
+	}
+
+}
