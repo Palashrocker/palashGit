a1c561ad894b33c7361ecd219b1d0dc98c2bd757
Palashrocker <54399517+Palashrocker@users.noreply.github.com>
2019-08-27 17:25:39 +0530

    Revert "test"

--- Branch Git/src/com/git/com/TestPush.java
+++ Branch Git/src/com/git/com/TestPush.java
@@ -4,7 +4,7 @@ public class TestPush {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Push");
+		System.out.println("Testing Push");
 		
 
 	}
